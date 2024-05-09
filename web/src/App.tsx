299af5d206d9115e1464e182e838c4dac6147975
Marcio Silva <marcio.silva.ter@estacio.br>
2024-05-09 13:08:04 -0300

    ajustes

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+
+  const enderecoRequicao = 'http://localhost:3000/livro';
+
+  const [livros, setLivros] = useState([]);
+  const [jaCarregouLivros, setJaCarregouLivros] = useState(false)
+
+  const carregaLivros = () => {
+    fetch(enderecoRequicao)
+      .then(resposta => {
+
+        if(!resposta.ok) { return Promise.reject(new Error('Deu erro, se vira!')) }
+
+        return resposta.json();
+      })
+      .then(dados => {
+        setLivros(dados);
+        setJaCarregouLivros(true);
+      })
+      .catch(error => { 
+        setJaCarregouLivros(false);
+        console.log(error.message)
+      } );
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={carregaLivros}>Carrega Livros</button>
+      {jaCarregouLivros && livros.length === 0 && <h3>Nenhum livro foi encontrado!</h3>}
+      {!jaCarregouLivros && <h3>Clique no botão para carregar os livros</h3>}
+      {jaCarregouLivros && livros.map((livro: any) => (
+        <div key={livro.id}>
+          <h2>{livro.titulo}</h2>
+          <strong>{livro.nomeAutor}</strong>
+        </div>
+      ))}
     </div>
   );
 }
